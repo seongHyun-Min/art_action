9b95e24b1cb8e3f1dbc94f6e8e9d177039512660
seongHyun-Min <steve6133@naver.com>
2023-07-17 17:51:40 +0900

    feat : UserController 추가

---
+++ src/main/java/com/example/artaction/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.example.artaction.controller;
+
+
+import com.example.artaction.domain.entity.User;
+import com.example.artaction.dto.user.UserRequestDto;
+import com.example.artaction.dto.user.UserResponseDto;
+import com.example.artaction.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Long> createUser(@Valid @RequestBody UserRequestDto requestDto) {
+        User saveUser = userService.save(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveUser.getId());
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Long> updateUser(
+            @PathVariable Long userId,
+            @Valid @RequestBody UserRequestDto requestDto) {
+        User updateUser = userService.update(userId, requestDto);
+
+        return ResponseEntity.ok((updateUser.getId()));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.delete(userId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId) {
+        User findUser = userService.findById(userId);
+
+        UserResponseDto responseUser = UserResponseDto.builder()
+                .id(findUser.getId())
+                .name(findUser.getName())
+                .email(findUser.getEmail())
+                .userType(findUser.getUserType())
+                .build();
+
+        return ResponseEntity.ok(responseUser);
+    }
+
+
+}
+
+
+
