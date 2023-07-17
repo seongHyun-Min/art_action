03ad8f5fd734f3298455aff1b3bb0b9a52d39ebf
seongHyun-Min <steve6133@naver.com>
2023-07-17 15:56:04 +0900

    feat : UserRequestDto 생성

---
+++ src/main/java/com/example/artaction/dto/user/UserRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.artaction.dto.user;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto {
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private Integer userType;
+
+}
