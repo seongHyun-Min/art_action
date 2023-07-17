1eb39b1570035debce1f452c3c82705a82ee05d2
seongHyun-Min <steve6133@naver.com>
2023-07-17 22:26:41 +0900

    feat : ArtWork 엔티티 추가

---
+++ src/main/java/com/example/artaction/domain/entity/ArtWork.java
@@ -0,0 +1,40 @@
+package com.example.artaction.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "art_works")
+public class ArtWork {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "art_work_id")
+    private long id;
+
+    @Column(name = "art_work_name")
+    private String name;
+
+    @Column(name = "art_work_description")
+    private String description;
+
+    @Column(name = "art_work_image")
+    private String image;
+
+    //유저와 대다일 관계 설정
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
