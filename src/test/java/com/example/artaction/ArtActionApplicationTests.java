7829928c3674015e1c2594f0d612ee0b649810f3
seongHyun-Min <steve6133@naver.com>
2023-07-17 14:13:59 +0900

    init : 프로젝트 생성

---
+++ src/test/java/com/example/artaction/ArtActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
