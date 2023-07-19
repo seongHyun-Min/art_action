8f5a173ad0765186346df66b923bb4a6fa8f1324
seongHyun-Min <steve6133@naver.com>
2023-07-19 17:54:11 +0900

    feat : AuctionException 추가

---
+++ src/main/java/com/example/artaction/exception/auction/NotSaveAuctionException.java
@@ -0,0 +1,8 @@
+package com.example.artaction.exception.auction;
+
+public class NotSaveAuctionException extends RuntimeException {
+
+    public NotSaveAuctionException(String message) {
+        super(message);
+    }
+}
