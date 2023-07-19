2aeb0ca02c624997e6671fc8b31ef73d1b071bf8
seongHyun-Min <steve6133@naver.com>
2023-07-19 13:53:47 +0900

    feat : Bid 엔티티 추가

---
+++ src/main/java/com/example/artaction/domain/entity/Bid.java
@@ -0,0 +1,39 @@
+package com.example.artaction.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bid")
+public class Bid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bid_id")
+    private Long id;
+
+    @Column(name = "bid_price")
+    private long price;
+
+    @Column(name = "bid_time")
+    private LocalDateTime bidTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "art_work_id")
+    private Action action;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
