8b30305e611ef17d2cbc1bd9580b56b59cc46c27
seongHyun-Min <steve6133@naver.com>
2023-07-19 17:53:40 +0900

    feat : AuctionRepository 기능 추가 유저로 등록한 경매 찾기

--- src/main/java/com/example/artaction/domain/repository/AuctionRepository.java
+++ src/main/java/com/example/artaction/domain/repository/AuctionRepository.java
@@ -2,6 +2,7 @@ package com.example.artaction.domain.repository;
 
 import com.example.artaction.contant.ActionStatus;
 import com.example.artaction.domain.entity.Auction;
+import com.example.artaction.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,8 @@ import java.util.Optional;
 
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
     Optional<Auction> findById(Long id);
+
+    Optional<List<Auction>> findByUser(User user);
 
     List<Auction> findByStatusAndStartTimeAfter(ActionStatus status, LocalDateTime StartTime);
 
