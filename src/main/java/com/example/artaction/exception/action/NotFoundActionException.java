2be4d27247f4d605fa481bdd399c06801aba5577
seongHyun-Min <steve6133@naver.com>
2023-07-19 15:01:23 +0900

    feat : ActionException 추가

---
+++ src/main/java/com/example/artaction/exception/action/NotFoundActionException.java
@@ -0,0 +1,8 @@
+package com.example.artaction.exception.action;
+
+public class NotFoundActionException extends RuntimeException {
+
+    public NotFoundActionException(String message) {
+        super(message);
+    }
+}
