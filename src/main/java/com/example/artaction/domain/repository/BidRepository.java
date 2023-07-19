448e1c5ea7c1c57bec6893d10695d78395a034c6
seongHyun-Min <steve6133@naver.com>
2023-07-19 14:14:39 +0900

    feat : Bid 리포지토리 추가

---
+++ src/main/java/com/example/artaction/domain/repository/BidRepository.java
@@ -0,0 +1,20 @@
+package com.example.artaction.domain.repository;
+
+import com.example.artaction.domain.entity.Action;
+import com.example.artaction.domain.entity.Bid;
+import com.example.artaction.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BidRepository extends JpaRepository<Bid, Long> {
+    Optional<Bid> findById(Long id);
+    Optional<List<Bid>> findByAction(Action action);
+    Optional<List<Bid>> findByUser(User user);
+
+    @Query("SELECT b FROM Bid b WHERE b.action = :action ORDER BY b.bidTime DESC")
+    List<Bid> findTop5ByActionOrderByBidTimeDesc(@Param("action") Action action);
+}
