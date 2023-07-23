6e8e60387aa9a3412814bb9e09c67002cc138d1d
seongHyun-Min <steve6133@naver.com>
2023-07-23 17:53:12 +0900

    refactor : AuctionService 반환 로직 변경 및 스케줄러 로직 분할

--- src/main/java/com/example/artaction/service/AuctionService.java
+++ src/main/java/com/example/artaction/service/AuctionService.java
@@ -6,8 +6,8 @@ import com.example.artaction.domain.entity.ArtWork;
 import com.example.artaction.domain.repository.AuctionRepository;
 import com.example.artaction.domain.repository.ArtWorkRepository;
 import com.example.artaction.domain.repository.BidRepository;
+import com.example.artaction.dto.auction.AuctionResponseDto;
 import com.example.artaction.dto.auction.PostAuctionRequestDto;
-import com.example.artaction.exception.auction.NotFoundAuctionException;
 import com.example.artaction.exception.auction.NotSaveAuctionException;
 import com.example.artaction.exception.artwork.NotFoundArtWorkException;
 import com.example.artaction.exception.artwork.NotSaveArtWorkException;
@@ -30,22 +30,23 @@ public class AuctionService {
     private final BidRepository bidRepository;
 
     @Transactional
-    public Auction post(PostAuctionRequestDto requestDto) {
+    public Long post(PostAuctionRequestDto requestDto) {
         ArtWork artWork = findArtWorkById(requestDto.getArtWorkId());
-
-        Auction auction = Auction.builder()
-                .artWork(artWork)
-                .startTime(requestDto.getStartTime())
-                .endTime(requestDto.getEndTime())
-                .startingPrice(requestDto.getStartingPrice())
-                .status(ActionStatus.PREPARE)
-                .build();
-
+        Auction auction = requestDto.toEntity(artWork);
         try {
-            return auctionRepository.save(auction);
+            return auctionRepository.save(auction).getId();
         } catch (NotSaveArtWorkException e) {
             throw new NotSaveAuctionException("경매 등록에 실패 하였습니다");
         }
+    }
+
+    @Transactional(readOnly = true)
+    public List<AuctionResponseDto> findByArtWork(Long artWorkId) {
+        ArtWork artWork = findByArtWorkId(artWorkId);
+        return auctionRepository.findByArtWork(artWork)
+                .stream()
+                .map(Auction::from)
+                .toList();
     }
 
     @Scheduled(fixedRate = 3600000) // 1시간마다 실행 (3600000ms = 1시간)
@@ -56,46 +57,30 @@ public class AuctionService {
         List<Auction> endAuctions = auctionRepository.findByStatusAndEndTimeAfter(ActionStatus.START, currentTime);
 
         for (Auction auction : startAuctions) {
-
-            Auction startAuction = Auction.builder()
-                    .artWork(auction.getArtWork())
-                    .startTime(auction.getStartTime())
-                    .endTime(auction.getEndTime())
-                    .startingPrice(auction.getStartingPrice())
-                    .currentPrice(auction.getCurrentPrice())
-                    .status(ActionStatus.START)
-                    .build();
-
+            Auction startAuction = getBuild(auction, ActionStatus.START, auction.getCurrentPrice());
             auctionRepository.save(startAuction);
         }
+
         for (Auction auction : endAuctions) {
-
             Optional<Integer> maxPrice = bidRepository.findTop1ByAuctionOrderByPriceDesc(auction);
-            ActionStatus actionStatus = ActionStatus.END;
-            if (!maxPrice.isPresent()) {
-                actionStatus = ActionStatus.FAIL;
-            }
+            ActionStatus actionStatus = maxPrice.isPresent() ? ActionStatus.END : ActionStatus.FAIL;
+            long endPrice = maxPrice.orElse(0).longValue();
 
-            Auction endAuction = Auction.builder()
-                    .artWork(auction.getArtWork())
-                    .startTime(auction.getStartTime())
-                    .endTime(auction.getEndTime())
-                    .startingPrice(auction.getStartingPrice())
-                    .currentPrice(maxPrice.orElse(0))
-                    .status(actionStatus)
-                    .build();
+            Auction endAuction = getBuild(auction, actionStatus, endPrice);
             auctionRepository.save(endAuction);
-
         }
     }
 
-    @Transactional(readOnly = true)
-    public List<Auction> findByArtWork(Long artWorkId) {
-        ArtWork artWork = findByArtWorkId(artWorkId);
-        return auctionRepository.findByArtWork(artWork)
-                .orElseThrow(() -> new NotFoundAuctionException("옥션 등록 기록이 존재하지 않습니다"));
+    private static Auction getBuild(Auction auction, ActionStatus status, long price) {
+        return Auction.builder()
+                .artWork(auction.getArtWork())
+                .startTime(auction.getStartTime())
+                .endTime(auction.getEndTime())
+                .startingPrice(auction.getStartingPrice())
+                .currentPrice(price)
+                .status(status)
+                .build();
     }
-
 
     private ArtWork findByArtWorkId(Long artWorkId) {
         return artWorkRepository.findById(artWorkId)
