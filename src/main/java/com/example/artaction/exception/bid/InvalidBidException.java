f036bbd71d26210da79fd48f0aaee75404671148
seongHyun-Min <steve6133@naver.com>
2023-07-23 18:51:21 +0900

    chore : exception 클래스 명 변경

---
+++ src/main/java/com/example/artaction/exception/bid/InvalidBidException.java
@@ -0,0 +1,8 @@
+package com.example.artaction.exception.bid;
+
+public class InvalidBidException extends IllegalArgumentException {
+
+    public InvalidBidException(String s) {
+        super(s);
+    }
+}
