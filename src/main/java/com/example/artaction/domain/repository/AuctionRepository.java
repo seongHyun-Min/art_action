1c210fc20d32ee78625a64fefa48c5f22407b954
seongHyun-Min <steve6133@naver.com>
2023-07-23 17:52:49 +0900

    refactor : List Optional 제거

--- src/main/java/com/example/artaction/domain/repository/AuctionRepository.java
+++ src/main/java/com/example/artaction/domain/repository/AuctionRepository.java
@@ -3,7 +3,6 @@ package com.example.artaction.domain.repository;
 import com.example.artaction.contant.ActionStatus;
 import com.example.artaction.domain.entity.ArtWork;
 import com.example.artaction.domain.entity.Auction;
-import com.example.artaction.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -13,7 +12,7 @@ import java.util.Optional;
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
     Optional<Auction> findById(Long id);
 
-    Optional<List<Auction>> findByArtWork(ArtWork artWork);
+    List<Auction> findByArtWork(ArtWork artWork);
 
     List<Auction> findByStatusAndStartTimeAfter(ActionStatus status, LocalDateTime StartTime);
 
