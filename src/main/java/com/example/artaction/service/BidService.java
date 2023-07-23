ac10435e9c82a9f227244614972631fa98e3d2d9
seongHyun-Min <steve6133@naver.com>
2023-07-23 18:52:17 +0900

    refactor : 반환 로직 변경 및 검증 로직 추가

--- src/main/java/com/example/artaction/service/BidService.java
+++ src/main/java/com/example/artaction/service/BidService.java
@@ -6,10 +6,11 @@ import com.example.artaction.domain.entity.User;
 import com.example.artaction.domain.repository.AuctionRepository;
 import com.example.artaction.domain.repository.BidRepository;
 import com.example.artaction.domain.repository.UserRepository;
-import com.example.artaction.dto.bid.CreateBidRequestDto;
+import com.example.artaction.dto.bid.AuctionBidResponseDto;
+import com.example.artaction.dto.bid.PostBidRequestDto;
+import com.example.artaction.dto.bid.UserBidResponseDto;
 import com.example.artaction.exception.auction.NotFoundAuctionException;
-import com.example.artaction.exception.bid.InvalidBidPriceException;
-import com.example.artaction.exception.bid.NotFoundBidException;
+import com.example.artaction.exception.bid.InvalidBidException;
 import com.example.artaction.exception.bid.NotSaveBidException;
 import com.example.artaction.exception.user.NotAuthorizedUserException;
 import com.example.artaction.exception.user.NotFoundUserException;
@@ -18,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,40 +32,29 @@ public class BidService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Bid create(CreateBidRequestDto requestDto) {
+    public Long Post(PostBidRequestDto requestDto) {
         User user = findUserById(requestDto.getUserId());
+
         if (!user.getUserType().isBuyer()) {
             throw new NotAuthorizedUserException("구매자 권한이 없습니다");
         }
 
         Auction auction = findActionById(requestDto.getAuctionId());
-        long bidPrice = requestDto.getPrice();
-        validateBidPrice(auction, bidPrice);
+        validateBidPrice(auction, requestDto.getPrice());
 
         Optional<Bid> existedBid = bidRepository.findByAuctionAndUser(auction, user);
 
         if (existedBid.isPresent()) {
-            Bid beforeBid = Bid.builder()
-                    .id(existedBid.get().getId())
-                    .auction(auction)
-                    .user(user)
-                    .price(bidPrice)
-                    .bidTime(LocalDateTime.now())
-                    .build();
+            Bid UpdateBid = requestDto.toUpdateEntity(existedBid.get().getId(), user, auction);
             try {
-                return bidRepository.save(beforeBid);
+                return bidRepository.save(UpdateBid).getId();
             } catch (NotSaveBidException e) {
                 throw new NotSaveBidException("입찰 갱신에 실패 하였습니다");
             }
         } else {
-            Bid newBid = Bid.builder()
-                    .auction(auction)
-                    .user(user)
-                    .price(bidPrice)
-                    .bidTime(LocalDateTime.now())
-                    .build();
+            Bid bid = requestDto.toEntity(user, auction);
             try {
-                return bidRepository.save(newBid);
+                return bidRepository.save(bid).getId();
             } catch (NotSaveBidException e) {
                 throw new NotSaveBidException("입찰 등록에 실패 하였습니다");
             }
@@ -73,17 +62,21 @@ public class BidService {
     }
 
     @Transactional(readOnly = true)
-    public List<Bid> findByUser(Long userId) {
+    public List<UserBidResponseDto> findByUser(Long userId) {
         User user = findUserById(userId);
         return bidRepository.findByUser(user)
-                .orElseThrow(() -> new NotFoundBidException("입찰 내역이 존재하지 않습니다."));
+                .stream()
+                .map(Bid::fromUser)
+                .toList();
     }
 
     @Transactional(readOnly = true)
-    public List<Bid> findTop5ByAction(Long actionId) {
+    public List<AuctionBidResponseDto> findTop5ByAuction(Long actionId) {
         Auction auction = findActionById(actionId);
-        return bidRepository.findTop5ByActionOrderByBidTimeDesc(auction)
-                .orElseThrow(() -> new NotFoundBidException("입찰 내역이 존재하지 않습니다"));
+        return bidRepository.findTop5ByAuctionOrderByBidTimeDesc(auction)
+                .stream()
+                .map(Bid::fromAuction)
+                .toList();
     }
 
     private User findUserById(Long userId) {
@@ -97,11 +90,14 @@ public class BidService {
     }
 
     private void validateBidPrice(Auction auction, long bidPrice) {
+        if (!auction.getStatus().isStart()) {
+            throw new InvalidBidException("경매가 진행중이 아닙니다.");
+        }
         if (bidPrice <= auction.getStartingPrice()) {
-            throw new InvalidBidPriceException("입찰금액이 시작 금액보다 낮습니다.");
+            throw new InvalidBidException("입찰금액이 시작 금액보다 낮습니다.");
         }
         if (bidPrice <= auction.getCurrentPrice()) {
-            throw new IllegalArgumentException("입찰금액이 현재 가격보다 낮습니다");
+            throw new InvalidBidException("입찰금액이 현재 가격보다 낮습니다");
         }
     }
 }
