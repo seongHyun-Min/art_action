4353a3c7770d1be926da40e97900cc448f07748e
seongHyun-Min <steve6133@naver.com>
2023-07-23 19:08:42 +0900

    refactor : 엔티티 변환 로직 추가

--- src/main/java/com/example/artaction/dto/user/UpdateUserRequestDto.java
+++ src/main/java/com/example/artaction/dto/user/UpdateUserRequestDto.java
@@ -1,8 +1,8 @@
 package com.example.artaction.dto.user;
 
+import com.example.artaction.domain.entity.User;
 import lombok.*;
 
-import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -16,6 +16,13 @@ public class UpdateUserRequestDto {
 
     private String password;
 
-    private Integer userType;
-
+    public User toEntity(User user) {
+        return User.builder()
+                .id(user.getId())
+                .name(this.userName != null ? this.userName : user.getName())
+                .email(this.email != null ? this.email : user.getEmail())
+                .password(this.password != null ? this.password : user.getPassword())
+                .userType(user.getUserType())
+                .build();
+    }
 }
