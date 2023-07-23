968aa680f7a1011627b9ce1a83bc2d0723505b0f
seongHyun-Min <steve6133@naver.com>
2023-07-23 19:10:11 +0900

    refactor : User 컨트롤러 불필요한 로직 제거

--- src/main/java/com/example/artaction/controller/UserController.java
+++ src/main/java/com/example/artaction/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.artaction.controller;
 
 
-import com.example.artaction.domain.entity.User;
 import com.example.artaction.dto.user.CreateUserRequestDto;
 import com.example.artaction.dto.user.UpdateUserRequestDto;
 import com.example.artaction.dto.user.UserResponseDto;
@@ -22,43 +21,28 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Long> createUser(@Valid @RequestBody CreateUserRequestDto requestDto) {
-        User saveUser = userService.save(requestDto);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(saveUser.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(requestDto));
     }
 
     @PutMapping("/{userId}")
     public ResponseEntity<Long> updateUser(
             @PathVariable Long userId,
             @Valid @RequestBody UpdateUserRequestDto requestDto) {
-        User updateUser = userService.update(userId, requestDto);
-
-        return ResponseEntity.ok((updateUser.getId()));
+        return ResponseEntity.ok((userService.update(userId, requestDto)));
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
         userService.delete(userId);
-
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId) {
-        User findUser = userService.findById(userId);
-
-        UserResponseDto responseUser = UserResponseDto.builder()
-                .id(findUser.getId())
-                .name(findUser.getName())
-                .email(findUser.getEmail())
-                .userType(findUser.getUserType())
-                .build();
-
-        return ResponseEntity.ok(responseUser);
+        return ResponseEntity.ok(userService.findById(userId));
     }
-
-
 }
+
 
 
 
